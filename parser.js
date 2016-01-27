c02dd1472cc0d9769990e94fd652439802aee1c9
Mark Gable <mgable43@gmail.com>
2016-01-26 17:08:32 -0800

    changed UUID to hashcode

--- parser.js
+++ parser.js
@@ -19,9 +19,10 @@
 
 	function myMap(data){
 		var obj = {};
-		obj.id = util.generateUID();
+		//obj.id = util.generateUID();
 		obj.title = removeDoubleEscape(data.children[1].data); //title
 		obj.link = decodeLink(data.attribs.href); //link to item
+		obj.id = util.generateHashCode(obj.link);
 		obj.meta = makeSaleData(removeDoubleEscape(data.attribs.x)); //selling price / time of sale / bids / watchers
 		obj.src = decodeLink(data.children[0].attribs.src); // image src
 
