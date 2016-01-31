5457cddb75a7d3f0c15cf1d40074447373d2694e
Mark Gable <mgable43@gmail.com>
2016-01-31 07:50:56 -0800

    typo in config

--- config/config.js
+++ config/config.js
@@ -6,7 +6,7 @@
 	exports.category = {name: 'advertising_tins', id: 1175};
 	exports.categories = [exports.category];
 	exports.domain = 'www.collectorsweekly.com';
-	exports.searchHost = 'HOST="https://search-mgable-es-ht4qtiycv6v543iujwxk6q5n3u.us-west-2.es.amazonaws.com/';
+	exports.searchHost = 'https://search-mgable-es-ht4qtiycv6v543iujwxk6q5n3u.us-west-2.es.amazonaws.com/';
 	exports.pageUrlTemplate = '/ajax/category-auctions.php?id= *** config.category.id *** &sort=completed&limit=1000&offset=0';
 	exports.contentType = {
 		'json': 'application/json; charset=UTF-8',
