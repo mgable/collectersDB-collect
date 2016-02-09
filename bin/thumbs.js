32478c8715bdf6454c2ae3b90793742f38a015a1
Mark Gable <mgable43@gmail.com>
2016-02-09 09:27:29 -0800

    fixed thumbnail and no additional images issue

--- bin/thumbs.js
+++ bin/thumbs.js
@@ -3,15 +3,18 @@
 (function(){
 	var exports = {};
 
+	// includes
 	var Q = require('q'),
 		util = require('../lib/util.js'),
 		upload = require('./upload.js');
 
+	// assignments
 	var filesReceived = 0,
 		totalItems,
 		items,
 		deferred = Q.defer();
 
+	// public methods
 	function fetchImages(data, imagePath){
 		// set class variables
 		items = data;
@@ -21,7 +24,7 @@
 		items.forEach(function(item){
 			var filename = item.src.local.replace(/^\d{4}\/\d{2}\/\d{2}\//, "");
 
-			upload.S3(item.src.original, imagePath, filename, thumbNailCallback);
+			upload.S3(item.src.original, imagePath, filename, _thumbNailCallback);
 		});
 
 		util.logger.log("info", "Fetching Thumbnails", {imageCount: totalItems, imagePath: imagePath});
@@ -29,7 +32,12 @@
 		return deferred.promise;
 	}
 
-	function thumbNailCallback(/* uri, imagePath, filename */){
+	function makeSmallerImageUrl(url){
+		return url.replace(/(?!s\-l)\d{2,3}/, "140");
+	}
+
+	// private methods
+	function _thumbNailCallback(/* uri, imagePath, filename */){
 		console.info("getting callback " + (filesReceived + 1) + " out of " + totalItems);
 		if (++filesReceived === totalItems){
 			util.logger.log("info", "Fetched Thumbnails", {imageCount: totalItems});
@@ -37,6 +45,7 @@
 		}
 	}
 
+	exports.makeSmallerImageUrl = makeSmallerImageUrl;
 	exports.fetchImages = fetchImages;
 
 	module.exports = exports;
