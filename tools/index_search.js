3bc3b2380aaad51ebab162c241966cade6856b1d
Mark Gable <mgable43@gmail.com>
2016-03-02 15:40:29 -0800

    lock down AWS ES

--- tools/index_search.js
+++ tools/index_search.js
@@ -10,11 +10,12 @@
 		fs = require('fs');
 
 	// assignments
-	var items = JSON.parse(fs.readFileSync(__dirname + '/TEST.data.json', 'utf8')),
+	var items = JSON.parse(fs.readFileSync(__dirname + '/advertising_tins_store.json', 'utf8')),
+		host = "http://search-collectors-db-k6k76eedtz272dx3t5eqsmo2wq.us-west-1.es.amazonaws.com/",
 		index = "collectorsdb";
 
 	// the process
-	make.makeIndex(items, index, true).then(function(data){
+	make.makeIndex(items, index, true, host).then(function(data){
 		console.info("done!!!");
 	});
 
